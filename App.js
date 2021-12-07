a640a60251f0dee04d04444fc5f6960c4bcd35b5
udaya raj urs G <Udayarajurs7@gmail.com>
2021-12-07 17:26:44 +0530

    working on

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { View , Text , StyleSheet ,  StatusBar} from 'react-native'
 import { colors } from './src/global/styles';
 import SigninScreen from './src/screens/authScreens/SigninScreen';
 
+import SigninWelcomeScreen from './src/screens/authScreens/SigninWelcomeScreen';
+
 
 export default function App(){
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.statusbar} />
 
-      <SigninScreen />
+      <SigninWelcomeScreen />
     </View>
   );
 }

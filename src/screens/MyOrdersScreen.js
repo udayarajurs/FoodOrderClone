b3f650f931de74e6f6c51d4201e14a481d8a4e74
udaya raj urs G <Udayarajurs7@gmail.com>
2021-12-15 17:26:26 +0530

    working

---
+++ src/screens/MyOrdersScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View , Text , StyleSheet} from "react-native";
+
+export default function MyOrdersScreen(){
+    return(
+        <View style={{flex:1, alignItems:'center',justifyContent: 'center'}}>
+                <Text>MyOrdersScreen</Text>
+        </View>
+    )
+}
---
+++ src/screens/SearchScreeen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View , Text , StyleSheet} from "react-native";
+
+export default function SearchScreeen(){
+    return(
+        <View style={{flex:1, alignItems:'center',justifyContent: 'center'}}>
+                <Text>SearchScreeen</Text>
+        </View>
+    )
+}
